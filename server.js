b2d5b8486e4e4946a30b0c7b6c12a8c61d58966a
nvhung1088-alt <nvhung1088@gmail.com>
2026-08-16 11:28:30 +0700

    Add facebook_photos array to Make webhook payload

--- server.js
+++ server.js
@@ -663,6 +663,7 @@ async function triggerMakeWebhook(blogData) {
             image3: imagesList[2] || imagesList[0],
             image4: imagesList[3] || imagesList[0],
             images: imagesList,
+            facebook_photos: imagesList.map(url => ({ url })),
             hashtags: smartHashtags,
             formatted_content: formattedContent,
             created_at: blogData.created_at || new Date().toISOString()
